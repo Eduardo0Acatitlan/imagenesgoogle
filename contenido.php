097b3359a41645641342ffede71f9f87f896bae3
eduardo <16106489@tecnmtlahuac.onmicrosoft.com>
2020-09-24 20:02:25 -0500

    primer commit

---
+++ contenido.php
@@ -0,0 +1,40 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/Raam.jpg"."||".
+				  "Raam"."||".
+				  "El General RAAM fue un silencioso y salvaje comandante de la Horda Locust. Era un Guardia Theron, pero rápidamente ganó la confianza de la Reina, era muy astuto y ambicioso ocupando su poder a través de las habilidad y el intelecto. RAAM también tenía una Espada, que con su gran tamaño hacía parecer un cuchillo. Es el único que puede controlar a los Kryl, y una nube de ellos funcionan como sus Guardaespaldas o escudo contra las balas. El General RAAM comandó las tropas durante la Ofensiva de Masa Ligera, tratando de salvar la Hondonada en vano. RAAM encontró su destino a manos de Marcus Fenix y Dominic Santiago a bordo del Pilar de Tyro.";
+
+
+		$datos[1]="img/Sniper.jpg"."||".
+				  "Sniper"."||".
+				  "El Francotirador es una variante del Dron de la Horda Locust. Estos están especializados en usar el Rifle de Largo Alcance en todas las batallas.";
+
+
+		$datos[2]="img/Kantus.jpg"."||".
+				  "Kantus"."||".
+				  "Los Kantus son sacerdotes guerreros, fervientes seguidores de la religión de la horda Locust: la trinidad de los gusanos. Su característico cántico de guerra es famoso por dar fuerzas y curar a los aliados caídos..";
+
+
+		$datos[3]="img/Dom.jpg"."||".
+				  "Dominic"."||".
+				  "Dominic (Dom) Santiago, soldado comando de las Fuerzas Armadas de la Coalición de Gobiernos Ordenados (CGO). Es un duro guerrero que posee una actitud positiva incluso en los peores momentos. Liberó a su mejor amigo, Marcus Fenix, de la Cárcel de Máxima Seguridad de Jacinto y lo reclutó para el pelotón Delta. Luego de perder todo por lo que luchaba, Dom se descuidó mucho, participó durante la Pandemia Lambent y se sacrifico para salvar a Delta en la Misión a Mercy.";
+
+		$datos[4]="img/kim.jpg"."||".
+				  "Minh Young Kim"."||".
+				  "Minh Young Kim, Teniente de las Fuerzas Armadas de la Coalición de Gobiernos Ordenados (CGO). Un oficial Gear que sirvió durante la Guerra Locust, sobre todo durante la Ofensiva de Masa Ligera. Un militar con un estilo de liderazgo “de libro”, Kim dirigió la misión para rescatar a Marcus Fenix de la Cárcel de Máxima Seguridad de Jacinto a pesar de sus reservas acerca de un hombre al que considera un desertor. Pero fue asesinado apenas unas horas después por el general RAAM.";
+
+
+		
+
+	    $datos[5]="img/Benjamin.jpg"."||".
+				  "Benjamin Carmine"."||".
+				  "Benjamin Carmine (Placa: CGO CSID 83B186-22AO3-SF), Soldado de las Fuerzas Armadas de la Coalición de Gobiernos Ordenados, fue un Gear que sirvió a la Coalición durante la Guerra Locust.";
+
+
+		return $datos;
+	}
+
+ ?>
